2918178b4260784a6eda7ea0f1e80c17e4af84f0
Max Barkley <max@dnastack.com>
2019-07-24 18:05:43 -0400

    Add ci build scripts and simple smoke test.
    (cherry picked from commit 4fc59ba038c73f4d231432d8c38869a117583417)

---
+++ e2e-tests/src/main/java/com/dnastack/ic/IcSmokeTest.java
@@ -0,0 +1,27 @@
+package com.dnastack.ic;
+
+import com.dnastack.BaseE2eTest;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class IcSmokeTest extends BaseE2eTest {
+
+    @Test
+    public void returnServiceInfo() {
+        given()
+            .log().method()
+            .log().uri()
+        .when()
+            .header("Accept", "application/json")
+            .get("/identity")
+        .then()
+            .log().ifValidationFails()
+            .statusCode(200)
+            .contentType(ContentType.JSON)
+            .body("name", equalTo("Identity Concentrator"))
+            .body("ui.label", not(isEmptyOrNullString()));
+    }
+}
